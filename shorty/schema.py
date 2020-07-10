9a796d344e271b635fe7fff9f4aa1bcbf1cd4db5
SStefanovskyi <sstefanovskiy@gmail.com>
2020-07-10 20:05:12 +0300

    Created mutation

--- shorty/schema.py
+++ shorty/schema.py
@@ -5,4 +5,7 @@ import shortener.schema
 class Query(shortener.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(shortener.schema.Mytation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
